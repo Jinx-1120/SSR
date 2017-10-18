1fffe9074d0b969e0d3788b0789749342679352d
jinhaidi1 <1753612833@qq.com>
2017-10-18 17:33:39 +0800

    test

--- build/main.js
+++ build/main.js
@@ -82,16 +82,13 @@ module.exports = {
   /*
   ** Router config
   */
-  router: {
-    // mode: 'hash'
-  },
   /*
   ** Headers of the page
   */
   head: {
     title: 'demo',
     meta: [{ charset: 'utf-8' }, { hid: 'description', name: 'description', content: 'Nuxt.js project' }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: 'stylesheet', href: '/common/common.css' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: 'stylesheet', href: 'https://unpkg.com/element-ui/lib/theme-default/index.css' }]
   },
   /*
   ** Customize the progress bar color
@@ -100,24 +97,20 @@ module.exports = {
   /*
   ** Build configuration
   */
-  vender: ['axios']
-  // babel: {
-  //   'plugins': [['component', [
+  vender: ['axios', 'element-ui'],
+  // babel:{
+  //   "plugins": [["component", [
   //     {
-  //       'libraryName': 'element-ui',
-  //       'styleLibraryName': 'theme-default'
+  //       "libraryName": "element-ui",
+  //       "styleLibraryName": "theme-default"
   //     },
-  //     'transform-async-to-generator'
+  //     'transform-async-to-generator',
+  //     'transform-runtime'
   //   ]]],
   //   comments: true
   // },
-  // plugins: [
-  //   { src: '~plugins/element-ui', ssr: true }
-  // ]
-  // css: [
-  //   'element-ui/lib/theme-default/index.css'
-  // ]
-
+  plugins: [{ src: '~plugins/element-ui', ssr: true }],
+  css: ['assets/main.css']
 };
 
 /***/ },
